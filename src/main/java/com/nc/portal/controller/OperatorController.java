2f410a659037abe4f073955b2ae144d271d4f577
nik1998 <beregnovnikita@gmail.com>
2019-03-10 20:42:07 +0300

    -nik
    Update
    Russian letters!!
    Bicycle!!!

--- src/main/java/com/nc/portal/controller/OperatorController.java
+++ src/main/java/com/nc/portal/controller/OperatorController.java
@@ -1,24 +1,56 @@
 package com.nc.portal.controller;
 
 import com.nc.portal.model.OrdersDTO;
+import com.nc.portal.model.UserDTO;
 import com.nc.portal.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 @Controller
+@RequestMapping(value = "/operator")
 public class OperatorController {
 
     @Autowired
     OrdersService ordersService;
-
+    @InitBinder//конвертер для пустой строки в дату
+    public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        // true passed to CustomDateEditor constructor means convert empty String to null
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));//пустая дата
+        binder.registerCustomEditor(String.class,new StringTrimmerEditor(true));//пустая строка
+    }
     @GetMapping
-    @RequestMapping(value = "/operator")
     public String getPage(Model model) {
-        OrdersDTO[] ob= ordersService.getAllOrders();
-        model.addAttribute("orders", ob);
-        return "operator";
+        if (UserDTO.getStaticRole().equals("OPERATOR")) {
+            OrdersDTO[] ordersDTO= ordersService.getAllOrders();
+            model.addAttribute("orders", ordersDTO);
+            model.addAttribute("order", new OrdersDTO());
+            return "operator";
+        }
+        model.addAttribute("userDTO", new UserDTO());
+        return "redirect:/auth";
+
+    }
+    @PostMapping
+    public String updateOrder(@ModelAttribute OrdersDTO order ,Model model) {
+        if (UserDTO.getStaticRole().equals("OPERATOR")) {
+            ordersService.updateOrders(order);
+            OrdersDTO[] ordersDTO= ordersService.getAllOrders();
+            model.addAttribute("orders", ordersDTO);
+            model.addAttribute("order", new OrdersDTO());
+            return "operator";
+        }
+        model.addAttribute("userDTO", new UserDTO());
+        return "redirect:/auth";
+
     }
 }
--- src/main/java/com/nc/portal/service/OrdersService.java
+++ src/main/java/com/nc/portal/service/OrdersService.java
@@ -1,15 +1,16 @@
 package com.nc.portal.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nc.portal.model.OrdersDTO;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 
 @Service
 public class OrdersService {
     private final String URL = "http://localhost:8082/orders";
+    private final ObjectMapper mapper = new ObjectMapper();
     private final RestTemplate restTemplate = new RestTemplate();
 
     public OrdersDTO[] getAllOrders() /*throws JSONException*/ {
@@ -20,7 +21,24 @@ public class OrdersService {
             return objects;
         } catch (Exception e) {
             System.out.println("** Exception: " + e.getMessage());
-            return null;
+            return new OrdersDTO[0];
+        }
+    }
+    public void updateOrders(OrdersDTO ordersDTO)
+    {
+        try {
+            HttpHeaders headers = new HttpHeaders();//new HttpHeaders();//createHttpHeaders(userDTO.getUsername(), userDTO.getPassword());
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            // create request body
+            String request = mapper.writeValueAsString(ordersDTO);
+
+            HttpEntity<String> entity = new HttpEntity<String>(request, headers);
+            ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.POST, entity, String.class);
+            System.out.println("Result - status " + response.getStatusCode());
+        }
+        catch (Exception e) {
+            System.out.println("** Exception: " + e.getMessage());
         }
     }
 }
