2e110f096d0366b3825626b544c8fddad2bd4912
tapu0815 <tusya_0119>
2019-03-29 12:28:42 +0300

    todo artem

--- src/main/java/com/nc/portal/controller/MainController.java
+++ src/main/java/com/nc/portal/controller/MainController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//todo artem
+//перенести портал на порт 8081
+//MainController переименовать в StartPageController; + переименовать метод, html file, css file;
+//@RequestMapping в MainController перенести над методом, @GetMapping - убрать
+
 @Controller
 @RequestMapping(value = "/")
 public class MainController {
